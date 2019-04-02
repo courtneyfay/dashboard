95268ac3c4aed4ad53f43432f6a4f898b89eb335
Courtney Fay <fay.courtney@gmail.com>
2019-04-01 18:01:57 -0600

    setting up store and using it to "get" data

--- src/store.js
+++ src/store.js
@@ -1,10 +1,19 @@
 import Vue from "vue";
 import Vuex from "vuex";
+//import axios from 'axios';
+//axios.defaults.withCredentials = true;
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    data: ""
+  },
   mutations: {},
-  actions: {}
+  actions: {
+    requestBigQueryData() {
+      const response = "got the data!";
+      this.state.data = response;
+    }
+  }
 });
--- src/views/Page1.vue
+++ src/views/Page1.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="page1">
     <h1>This is Page 1</h1>
+    <div>{{ $store.state.data }}</div>
   </div>
 </template>
+
+<script>
+export default {
+  beforeMount() {
+    this.$store.dispatch("requestBigQueryData");
+  }
+};
+</script>
